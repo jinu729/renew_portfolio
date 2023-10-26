4e47a1d77c8acb493b6fbf58c4309cdee470a31e
jinu729 <131428867+jinu729@users.noreply.github.com>
2023-10-26 12:38:54 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -25,3 +25,59 @@ $(function(){
             });
         });
     });           
+
+    //페이지 스크롤
+$('.page').on('mousewheel',function(e, d){
+    // console.log(e);
+    // d -> 휠 내릴때 -1. 휠 올릴때 1
+    if( d < 0){
+        var next = $(this).next().offset().top;
+        $('html, body').stop().animate({
+            "scrollTop":next
+        },1000);
+        
+    }else if(d > 0){
+        var prev = $(this).prev().offset().top;
+        $('html, body').stop().animate({
+            "scrollTop":prev
+        },1000);
+    }
+});
+
+
+
+
+// 하단바 마우스 진입시 모션
+$('.main_footer').on('mouseenter focus',function(){
+    $('.dock').css({bottom:'-150%'});
+    $('.dock').stop().animate({bottom:'0'},1250,function(){        
+    });
+});
+$('.main_footer').on('mouseleave focus',function(){
+    $('.dock').css({bottom:'0%'});
+    $('.dock').stop().animate({bottom:'-150%'},1250,function(){        
+    });
+});
+
+// skill 화면 
+// -> 스크롤링 시 자동 진행
+var scr = $(window).scrollTop();
+$(window).scroll(function(){
+    var scr = $(this).scrollTop();
+    // console.log(scr);
+    if(scr >= 2600){
+        $('.wrap .box .st1').addClass('add');
+    }else{
+        $('.wrap .box .st1').removeClass('add');
+    }
+    if(scr >= 2600){
+        $('.wrap .box .st2').addClass('add2');
+    }else{
+        $('.wrap .box .st2').removeClass('add2');
+    }
+    if(scr >= 2600){
+        $('.wrap .box .st3').addClass('add3');
+    }else{
+        $('.wrap .box .st3').removeClass('add3');
+    }
+});
